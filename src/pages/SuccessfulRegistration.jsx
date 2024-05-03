e1117d7988d38c59952ae1aa67c5fb59b0eadb88
alansshots <alengospodinov@gmail.com>
2024-05-03 10:18:14 +0200

    Register Confimation

--- src/pages/SuccessfulRegistration.jsx
+++ src/pages/SuccessfulRegistration.jsx
@@ -8,7 +8,7 @@ function SuccessfulRegistration() {
         <div class="flex flex-col items-center space-y-2">
           <CheckCircle className='text-green-600 w-28 h-28' />
           <h1 class="text-4xl font-bold">Thank You !</h1>
-          <p className='max-w-2xl'>Thank you for registering, please confirm your registration.</p>
+          <p className='max-w-2xl'>Thank you for registering! Please confirm your registration. An email has been sent to you.</p>
         </div>
       </div>
     </div>
